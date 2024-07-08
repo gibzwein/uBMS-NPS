a2b8316b54dcdcc116eb6fd9fa838bb6821c26ab
gibzwein <52330511+gibzwein@users.noreply.github.com>
2024-07-08 20:13:54 +0200

    No WiFi bricking fixed

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ def get_data():
             response = requests.get(url, headers=headers, timeout = 10)
             if response.status_code == 200:
                 print('Data fetched')
+                led.value(0)
                 gc.collect()
                 try:
                     return json.loads(response.text)
@@ -67,8 +68,7 @@ def get_data():
                     time.sleep(5 * retry_count)
                 else:
                     print("Exceeded maximum number of data retrieval attempts.")
-            led.value(0)
-            return None
+                    return None
         except Exception as e:
             print(f"An error occurred while retrieving data: {e}")
             retry_count += 1
@@ -78,6 +78,7 @@ def get_data():
             else:
                 print("Exceeded maximum number of data retrieval attempts.")
                 return None
+    return None
 
 def parse_data(json_data):
     parsed_data = []
